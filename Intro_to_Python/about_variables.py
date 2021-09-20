6a260c64a1e36d4902ad6ba2d534e4ceb079ad43
Anuja Deokar <anujadeokar28@gmail.com>
2021-09-20 17:40:52 +0530

    Assigning values to variable

---
+++ Intro_to_Python/about_variables.py
@@ -0,0 +1,24 @@
+"""
+    Variables:
+        Names given to computer memory locations.
+        Used to store values.
+        Value stored in variable is changeable.
+        
+    Rules:
+        Must start with a letter or the underscore character.
+        Can only contain A-z, 0-9, and _ .
+        Variable names are case-sensitive.
+        The keywords cannot be used naming the variable.
+"""
+# variable creation and single assignment
+name = 'Aditi'
+
+# multiple assignment with same value
+p = q = r = 20
+
+# multiple assignment with different values
+x, y, z = 10, "Python", 30.5
+
+# swap variables
+a, b = 10, 50
+a, b = b, a
