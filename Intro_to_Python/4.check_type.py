3b1a536d1fadfb14c7885af41c4c37d159f4cdbb
Anuja Deokar <anujadeokar28@gmail.com>
2021-09-20 17:59:27 +0530

    Primitive types and type() function

---
+++ Intro_to_Python/4.check_type.py
@@ -0,0 +1,33 @@
+"""
+    Python is dynamically typed language. 
+    
+    type():
+        Function used to check data type
+    Syntax:
+        type(check_object)
+    Return:
+        class type of the argument
+"""
+# x holding 'int' type of data
+x = 10
+print(type(x))
+
+# x holding 'float' type of data
+x = 30.55
+print(type(x))
+
+# x holding 'complex' type of data
+x = 20+30j
+print(type(x))
+
+# x holding 'bool' type of data
+x = True
+print(type(x))
+
+# x holding 'str' type of data
+x = 'Python'
+print(type(x))
+
+# None type
+x = None
+print(type(x))
