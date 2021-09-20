52dcd45f329194b8a3aba62b854af64be07b0d03
Anuja Deokar <anujadeokar28@gmail.com>
2021-09-20 19:08:19 +0530

    input() function

---
+++ Intro_to_Python/5.about_input.py
@@ -0,0 +1,30 @@
+"""
+        input():
+            Used to accept data from keyboard.
+        Syntax:
+            input(prompt)
+        Parameter:
+            prompt  Message to be displayed for accepting input.[optional]
+        Return type: str
+        Returns:
+            input from user
+"""
+# input() without prompt
+input()
+
+# input() with prompt
+input("Enter your name:")
+
+# store input
+name = input("Enter your name:")
+print(f"Hello {name} !")
+
+# check return type
+print(type(name))
+
+number = input("Enter numeric value:")
+print(f"Type of numeric value is {type(number)}")
+
+# convert str to int [Type casting]
+number = int(input("Enter numeric value:"))
+print(f"Type check after type casting:{type(number)}")
