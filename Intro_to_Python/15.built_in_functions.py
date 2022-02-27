b6fc4d0a3b49aa433ecfac55317cac352b6c05b4
Anuja <anujadeokar28@gmail.com>
2022-02-27 15:08:21 +0530

    Built in functions

---
+++ Intro_to_Python/15.built_in_functions.py
@@ -0,0 +1,15 @@
+"""
+    Program to demonstrate use of Built in functions
+    max()
+    min()
+"""
+# max from more than one values
+print(max(3,2,6,7))
+#max from single list
+print(max([8,4,5,3]))
+
+# min from more than one values
+print(min(3,2,6,7))
+
+#min from single list
+print(min([8,4,5,3]))
